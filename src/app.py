840c51de764b10fd276b0691b3667cbf350da718
miguel <miguelconde121004@gmail.com>
2023-05-16 19:09:55 -0300

    primeiro protótipo da interface navegável

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask ('__name__')
+
+@app.route('/')
+def home():
+    return render_template('home.html', is_home=True)
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/informacoeseducacionais')
+def informacoeseducacionais():
+    return render_template('informacoeseducacionais.html', is_informacoeseducacionais=True)
+
+@app.route('/interesses')
+def interesses():
+    return render_template('interesses.html', is_interesses=True)
+
+app.run(debug=True)
